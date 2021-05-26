596351f9148a771b1f686b2a4800b0bb7e2925eb
AllenHongjun <652971723@163.com>
2021-05-26 14:24:27 +0800

    权限配置授权

--- aspnet-core/src/TigerAdmin.Application.Contracts/Permissions/TigerAdminPermissionDefinitionProvider.cs
+++ aspnet-core/src/TigerAdmin.Application.Contracts/Permissions/TigerAdminPermissionDefinitionProvider.cs
@@ -10,31 +10,33 @@ namespace TigerAdmin.Permissions
     {
         public override void Define(IPermissionDefinitionContext context)
         {
-            var identityGroup = context.AddGroup(TigerAdminPermissions.GroupName);
+            //var identityGroup = context.AddGroup(TigerAdminPermissions.GroupName);
+
+            var identityGroup = context.GetGroup(IdentityPermissions.GroupName);
 
             //Define your own permissions here. Example:
             //myGroup.AddPermission(TigerAdminPermissions.MyPermission1, L("Permission:MyPermission1"));
-            var booksPermission = identityGroup.AddPermission(TigerAdminPermissions.Books.Default, L("Permission:Books"));
-            booksPermission.AddChild(TigerAdminPermissions.Books.Create, L("Permission:Book.Create"));
-            booksPermission.AddChild(TigerAdminPermissions.Books.Edit, L("Permission:Book.Edit"));
-            booksPermission.AddChild(TigerAdminPermissions.Books.Delete, L("Permission:Book.Delete"));
+            //var booksPermission = identityGroup.AddPermission(TigerAdminPermissions.Books.Default, L("Permission:Books"));
+            //booksPermission.AddChild(TigerAdminPermissions.Books.Create, L("Permission:Book.Create"));
+            //booksPermission.AddChild(TigerAdminPermissions.Books.Edit, L("Permission:Book.Edit"));
+            //booksPermission.AddChild(TigerAdminPermissions.Books.Delete, L("Permission:Book.Delete"));
 
-            var ouPermission = identityGroup.AddPermission(HelloIdentityPermissions.OrganitaionUnits.Default, IdentityL("Permission:OrganitaionUnitManagement"));
-            ouPermission.AddChild(HelloIdentityPermissions.OrganitaionUnits.Create, IdentityL("Permission:Create"));
-            ouPermission.AddChild(HelloIdentityPermissions.OrganitaionUnits.Update, IdentityL("Permission:Edit"));
-            ouPermission.AddChild(HelloIdentityPermissions.OrganitaionUnits.Delete, IdentityL("Permission:Delete"));
+            var ouPermission = identityGroup.AddPermission(TigerIdentityPermissions.OrganitaionUnits.Default, L("Permission:OrganitaionUnitManagement"));
+            ouPermission.AddChild(TigerIdentityPermissions.OrganitaionUnits.Create, L("Permission:Create"));
+            ouPermission.AddChild(TigerIdentityPermissions.OrganitaionUnits.Update, L("Permission:Edit"));
+            ouPermission.AddChild(TigerIdentityPermissions.OrganitaionUnits.Delete, L("Permission:Delete"));
 
             //var userPermission = identityGroup.GetPermissionOrNull(IdentityPermissions.Users.Default);
-            //userPermission?.AddChild(HelloIdentityPermissions.Users.DistributionOrganizationUnit, IdentityL("Permission:DistributionOrganizationUnit"));
+            //userPermission?.AddChild(TigerIdentityPermissions.Users.DistributionOrganizationUnit, IdentityL("Permission:DistributionOrganizationUnit"));
 
             //var rolePermission = identityGroup.GetPermissionOrNull(IdentityPermissions.Roles.Default);
-            //rolePermission?.AddChild(HelloIdentityPermissions.Roles.AddOrganizationUnitRole, IdentityL("Permission:AddOrganizationUnitRole"));
+            //rolePermission?.AddChild(TigerIdentityPermissions.Roles.AddOrganizationUnitRole, IdentityL("Permission:AddOrganizationUnitRole"));
 
             ////Claim
-            //var claimPermission = identityGroup.AddPermission(HelloIdentityPermissions.ClaimTypes.Default, IdentityL("Permission:ClaimManagement"));
-            //claimPermission.AddChild(HelloIdentityPermissions.ClaimTypes.Create, IdentityL("Permission:Create"));
-            //claimPermission.AddChild(HelloIdentityPermissions.ClaimTypes.Update, IdentityL("Permission:Edit"));
-            //claimPermission.AddChild(HelloIdentityPermissions.ClaimTypes.Delete, IdentityL("Permission:Delete"));
+            //var claimPermission = identityGroup.AddPermission(TigerIdentityPermissions.ClaimTypes.Default, IdentityL("Permission:ClaimManagement"));
+            //claimPermission.AddChild(TigerIdentityPermissions.ClaimTypes.Create, IdentityL("Permission:Create"));
+            //claimPermission.AddChild(TigerIdentityPermissions.ClaimTypes.Update, IdentityL("Permission:Edit"));
+            //claimPermission.AddChild(TigerIdentityPermissions.ClaimTypes.Delete, IdentityL("Permission:Delete"));
 
             ////AuditLogging
             //var auditLogGroup = context.AddGroup(AuditLogPermissions.GroupName);
