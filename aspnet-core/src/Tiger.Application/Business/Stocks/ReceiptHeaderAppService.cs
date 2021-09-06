94534a23fb3b12ed01fd9cf78cdf870ea9427509
AllenHongjun <652971723@163.com>
2021-09-06 23:13:45 +0800

    入库单crud优化

--- aspnet-core/src/Tiger.Application/Business/Stocks/ReceiptHeaderAppService.cs
+++ aspnet-core/src/Tiger.Application/Business/Stocks/ReceiptHeaderAppService.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using Tiger.Business.Stocks;
 using Tiger.Domain.CoreModule.Utilities;
@@ -15,12 +16,6 @@ namespace Tiger.Stock
     public class ReceiptHeaderAppService : CrudAppService<ReceiptHeader, ReceiptHeaderDto, Guid, PagedAndSortedResultRequestDto, CreateUpdateReceiptHeaderDto, CreateUpdateReceiptHeaderDto>,
         IReceiptHeaderAppService
     {
-        //protected override string GetPolicyName { get; set; } = TigerPermissions.ReceiptHeader.Default;
-        //protected override string GetListPolicyName { get; set; } = TigerPermissions.ReceiptHeader.Default;
-        //protected override string CreatePolicyName { get; set; } = TigerPermissions.ReceiptHeader.Create;
-        //protected override string UpdatePolicyName { get; set; } = TigerPermissions.ReceiptHeader.Update;
-        //protected override string DeletePolicyName { get; set; } = TigerPermissions.ReceiptHeader.Delete;
-
         private readonly IReceiptHeaderRepository _repository;
         private readonly IRepository<ReceiptDetail,Guid> _receiptDetailRepository;
         private readonly IReverseDetailRepository _reverseDetailRepository;
@@ -45,39 +40,27 @@ namespace Tiger.Stock
             {
                 item.Id = GuidGenerator.Create();
             }
-
             return await base.CreateAsync(input);
         }
 
         public override async Task<ReceiptHeaderDto> UpdateAsync(Guid id, CreateUpdateReceiptHeaderDto input)
         {
-            DeleteDetail(id);
-
-            //receiptHeader.ReceiptDetails.Clear();
-            //await CurrentUnitOfWork.CompleteAsync();
+            await DeleteDetail(id);
             foreach (var item in input.ReceiptDetails)
             {
                 item.Id = GuidGenerator.Create();
             }
-
-            //ObjectMapper.Map(input, receiptHeader);
-
-            //await _repository.UpdateAsync(receiptHeader, autoSave: true);
-
             return await base.UpdateAsync(id, input);
-            //return ObjectMapper.Map<ReceiptHeader, ReceiptHeaderDto>(receiptHeader);
         }
 
 
         public override async Task DeleteAsync(Guid id)
         {
-            DeleteDetail(id);
-            //await CurrentUnitOfWork.CompleteAsync();
-            //await _repository.UpdateAsync(receiptHeader, true);
+            await DeleteDetail(id);
             await base.DeleteAsync(id);
         }
 
-        private async void DeleteDetail(Guid id)
+        private async Task  DeleteDetail(Guid id)
         {
             var receiptHeader = await _repository.GetAsync(id);
             foreach (var receiptDetail in receiptHeader.ReceiptDetails)
@@ -91,57 +74,6 @@ namespace Tiger.Stock
             var query = await _repository.GetAsync(id);
             return ObjectMapper.Map<ReceiptHeader, ReceiptHeaderDto>(query); 
         }
-
-
-        //private async Task SaveTags(ICollection<string> newTags, Post post)
-        //{
-        //    await RemoveOldTags(newTags, post);
-
-        //    await AddNewTags(newTags, post);
-        //}
-
-        //private async Task RemoveOldTags(ICollection<string> newTags, Post post)
-        //{
-        //    foreach (var oldTag in post.Tags.ToList())
-        //    {
-        //        var tag = await _tagRepository.GetAsync(oldTag.TagId);
-
-        //        var oldTagNameInNewTags = newTags.FirstOrDefault(t => t == tag.Name);
-
-        //        if (oldTagNameInNewTags == null)
-        //        {
-        //            post.RemoveTag(oldTag.TagId);
-
-        //            tag.DecreaseUsageCount();
-        //            await _tagRepository.UpdateAsync(tag);
-        //        }
-        //        else
-        //        {
-        //            newTags.Remove(oldTagNameInNewTags);
-        //        }
-        //    }
-        //}
-
-        //private async Task AddNewTags(IEnumerable<string> newTags, Post post)
-        //{
-        //    var tags = await _tagRepository.GetListAsync(post.BlogId);
-
-        //    foreach (var newTag in newTags)
-        //    {
-        //        var tag = tags.FirstOrDefault(t => t.Name == newTag);
-
-        //        if (tag == null)
-        //        {
-        //            tag = await _tagRepository.InsertAsync(new Tag(GuidGenerator.Create(), post.BlogId, newTag, 1));
-        //        }
-        //        else
-        //        {
-        //            tag.IncreaseUsageCount();
-        //            tag = await _tagRepository.UpdateAsync(tag);
-        //        }
-
-        //        post.AddTag(tag.Id);
-        //    }
-        //}
+       
     }
 }
--- aspnet-core/src/Tiger.EntityFrameworkCore/Business/Stocks/ReceiptHeaderRepository.cs
+++ aspnet-core/src/Tiger.EntityFrameworkCore/Business/Stocks/ReceiptHeaderRepository.cs
@@ -1,5 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using System;
+using System.Threading;
 using System.Threading.Tasks;
 using Tiger.EntityFrameworkCore;
 using Volo.Abp.Domain.Repositories.EntityFrameworkCore;
@@ -13,11 +14,33 @@ namespace Tiger.Stock
         {
         }
 
+        
+
         public  async Task<ReceiptHeader> GetAsync(Guid id)
         {
             return await DbSet.AsNoTracking()
                 .Include(x => x.ReceiptDetails)
                 .FirstOrDefaultAsync(x => x.Id == id);
+        }
+
+        public override Task<ReceiptHeader> GetAsync(Guid id, bool includeDetails = true, CancellationToken cancellationToken = default)
+        {
+            return base.GetAsync(id, includeDetails, cancellationToken);
+        }
+
+        public override Task<ReceiptHeader> InsertAsync(ReceiptHeader entity, bool autoSave = false, CancellationToken cancellationToken = default)
+        {
+            return base.InsertAsync(entity, autoSave, cancellationToken);
+        }
+
+        public override Task<ReceiptHeader> UpdateAsync(ReceiptHeader entity, bool autoSave = false, CancellationToken cancellationToken = default)
+        {
+            return base.UpdateAsync(entity, autoSave, cancellationToken);
+        }
+
+        public override Task DeleteAsync(ReceiptHeader entity, bool autoSave = false, CancellationToken cancellationToken = default)
+        {
+            return base.DeleteAsync(entity, autoSave, cancellationToken);
         }
     }
 }
