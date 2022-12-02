43a6cd497bfd3fdb6eb44be4a41844a73c8d62e7
hongjy <hongjy1991@gmail.com>
2022-12-02 19:02:34 +0800

    id4

---
+++ aspnet-core/src/Tiger.Domain/Volo/Abp/IdentityServer/IApiResourceRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Volo.Abp.IdentityServer
+{
+    public interface IApiResourceRepository : Volo.Abp.IdentityServer.ApiResources.IApiResourceRepository
+    {
+        Task<List<string>> GetNamesAsync(CancellationToken cancellationToken = default);
+    }
+}
