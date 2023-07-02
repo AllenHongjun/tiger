9dbaa176e937fb411c1b87a59ecf0d846f326ff1
hongjy <806815027@qq.com>
2023-07-02 10:16:02 +0800

    定义职位实体类

---
+++ aspnet-core/src/Tiger.Domain/Volo/Abp/Identity/Post/Post.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Volo.Abp;
+using Volo.Abp.Domain.Entities.Auditing;
+using Volo.Abp.MultiTenancy;
+
+namespace Tiger.Volo.Abp.Identity.Post
+{   
+    /// <summary>
+    /// 职位
+    /// </summary>
+    public class Post : FullAuditedAggregateRoot<Guid>,IMultiTenant, ISoftDelete
+    {
+        public Post()
+        {
+            /* 此构造函数用于反序列化/ORM使用 */
+        }
+
+        /// <summary>
+        /// 备注
+        /// </summary>
+        public string Remark { get; set; }
+
+        /// <summary>
+        /// 职位编码
+        /// </summary>
+        public string Code { get; set; }
+
+        /// <summary>
+        /// 职位名称
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// 排序
+        /// </summary>
+        public int Sort { get; set; }
+
+        /// <summary>
+        /// 状态启用/禁用
+        /// </summary>
+        public bool IsActive { get; set; }
+
+        /// <summary>
+        /// 租户Id
+        /// </summary>
+        public Guid? TenantId {get;set; } = Guid.Empty;
+
+    }
+
+}
