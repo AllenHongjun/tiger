710ca4781a64a04958aa14133790d477369d6248
洪俊裕 <hongj2000@outlook.com>
2023-07-18 13:57:24 +0800

    本地环境安装

---
+++ aspnet-core/src/Tiger.EntityFrameworkCore.DbMigrations/Migrations/20230717010609_20230717.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Tiger.Migrations
+{
+    public partial class _20230717 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
