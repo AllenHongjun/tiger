42cb8559afe2640e7c69b8da36dd60bb151e8a7b
hongjy <806815027@qq.com>
2023-11-05 17:42:45 +0800

    题库表结构定义

--- aspnet-core/src/Tiger.Domain/Module/QuestionBank/QuestionAttachment.cs
+++ aspnet-core/src/Tiger.Domain/Module/QuestionBank/QuestionAttachment.cs
@@ -11,25 +11,30 @@ namespace Tiger.Module.QuestionBank
     public class QuestionAttachment:FullAuditedEntity<Guid>
     {
         /// <summary>
-        /// 试题题目ID
+        /// 试题题目Id
         /// </summary>
-        public Guid QuestionID { get; set; }
+        public Guid QuestionId { get; set; }
+
         /// <summary>
         /// 附件类型：1.内容，2.照片，3.文档，4.本地附件，5.本地视频，6.添加链接
         /// </summary>
         public QuestionAttachmentType AttachmentType { get; set; }
+
         /// <summary>
         /// 附件名称
         /// </summary>
         public string AttachmentTitle { get; set; }
+
         /// <summary>
         /// 附件内容
         /// </summary>
         public string AttachmentContent { get; set; }
+
         /// <summary>
         /// 排序
         /// </summary>
         public int Sorting { get; set; }
+
 
         public Question Question { get; set; }
     }
