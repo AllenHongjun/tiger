01657d4a9b1e9d8435bbe25b946d7b4403fdcfa7
hongjy <806815027@qq.com>
2023-11-21 20:37:51 +0800

    实操平台页面开发

--- vue-app/src/views/train-platform/index.vue
+++ vue-app/src/views/train-platform/index.vue
@@ -1,14 +1,17 @@
 <template>
   <div class="app-container">
     <el-row>
-      <el-col v-for="(o, index) in 10" :key="o" :span="8" :offset="index > 0 ? 2 : 0">
+      <el-col v-for="(o) in 10" :key="o" :span="8" :offset="2">
         <el-card :body-style="{ padding: '0px' }">
-          <img src="https://ali.srzx.com/img/sxpt.png" class="image">
+          <div class="image-box">
+            <img src="https://ali.srzx.com/img/sxpt.png" class="image">
+          </div>
+
+          <!-- <el-image src="https://ali.srzx.com/img/sxpt.png" :fit="scale-down" /> -->
           <div style="padding: 14px;">
-            <span>好吃的汉堡</span>
+            <b>Alibaba国际站实训平台</b>
             <div class="bottom clearfix">
-              <time class="time">{{ currentDate }}</time>
-              <el-button type="text" class="button">操作按钮</el-button>
+              <el-button type="primary" circle class="el-icon-right" />
             </div>
           </div>
         </el-card>
@@ -29,38 +32,50 @@ export default {
     }
   },
   methods: {
-    onSubmit() {
-      this.$message('submit!')
-    },
-    onCancel() {
-      this.$message({
-        message: 'cancel!',
-        type: 'warning'
-      })
-    }
   }
 }
 </script>
 
-<style scoped>
-.time {
-  font-size: 13px;
-  color: #999;
+<style lang="scss" scoped>
+
+.el-card{
+  height: 380px;
+  margin-bottom: 30px;
+  text-align: center;
+  color: #606266;
+  border-radius: 5%;
+  .image-box {
+    background-color: #eee;
+    width: 140px;
+    height: 140px;
+    line-height: 140px;
+    border-radius: 50%;
+    margin: 40px auto 20px;
+    display: block;
+    position: relative;
+
+    img{
+      position: absolute;
+      top: 50%;
+      left: 50%;
+      transform: translate(-50%,-50%);
+    }
+  }
+
+  .bottom{
+    margin-top: 40px;
+    line-height: 12px;
+  }
 }
 
-.bottom {
-  margin-top: 13px;
-  line-height: 12px;
+.el-card:hover{
+  background-color: #409EFF;
+  color: azure;
 }
 
-.button {
-  padding: 0;
-  float: right;
-}
-
-.image {
-  width: 160px;
-  display: block;
+.el-card:hover .el-button{
+  background-color: #eee;
+  color: #409EFF;
 }
 
 .clearfix:before,
