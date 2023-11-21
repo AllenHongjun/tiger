d1677eb162c0d805a11e365ba7104781c8d31ffb
洪俊裕 <hongj2000@outlook.com>
2023-11-21 11:37:17 +0800

    增加前台项目

---
+++ vue-admin/src/layout-web/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="app-container">
+    这是一个故意的空白页面
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'LayoutWeb',
+  data() {
+    return {
+      blank: {
+
+      }
+    }
+  },
+  methods: {
+
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
+
