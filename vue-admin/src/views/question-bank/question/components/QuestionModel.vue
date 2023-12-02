2e1331f5f3c309624d824bfbf052e0e9d5f9ddb3
洪俊裕 <hongj2000@outlook.com>
2023-12-02 17:22:37 +0800

    填空题答案添加保存渲染

--- vue-admin/src/views/question-bank/question/components/QuestionModel.vue
+++ vue-admin/src/views/question-bank/question/components/QuestionModel.vue
@@ -20,7 +20,7 @@
           </el-col>
           <el-col :span="12">
             <el-form-item :label="$t('AppQuestionBank[\'DisplayName:QuestionCateogryName\']')" prop="questionCategoryId">
-              <el-cascader v-model="temp.questionCategoryId" :options="questionCategoryOptions" :props="{ checkStrictly: true, value:'id', label:'name',children:'children',emitPath:false}" clearable class="filter-item" filterable />
+              <el-cascader v-model="temp.questionCategoryId" :options="questionCategoryOptions" :props="{ checkStrictly: true, value:'id', label:'name',children:'children',emitPath:false}" clearable class="filter-item" filterable style="width:100%;" />
             </el-form-item>
           </el-col>
         </el-row>
@@ -30,8 +30,8 @@
 
         <el-form-item :label="$t('AppQuestionBank[\'DisplayName:Content\']')" prop="content">
           <el-checkbox v-model="useEditor">使用富文本编辑器</el-checkbox>
-          <tinymce v-if="useEditor" v-model="temp.content" :height="300" />
-          <el-input v-else v-model="temp.content" type="textarea" :autosize="{ minRows: 4, maxRows: 6}" />
+          <tinymce v-if="useEditor" v-model="temp.content" :height="300" @blur="onInputBlur" />
+          <el-input v-else v-model="temp.content" type="textarea" :autosize="{ minRows: 4, maxRows: 6}" @blur="onInputBlur" />
           <div v-if="temp.type === QuestionType.Completion" style="margin-top: 10px;">
             <span>请在填空位置</span>   <el-button type="primary" @click="insertBlankFill()">插入填空符</el-button>
           </div>
@@ -65,7 +65,7 @@
             <el-col :span="20">选项内容</el-col>
           </el-row>
           <input v-model="temp.answer" type="hidden" name="multipleChoiceAnswer">
-          <el-checkbox-group v-model="temp.answerArr">
+          <el-checkbox-group v-model="answerArr">
             <el-form-item v-for="(item, index) in optionContentArr" :key="item.key" label="选项">
               <el-checkbox :label="alphas[index]" />
               <el-input v-model="item.value" />
@@ -80,18 +80,15 @@
         <!-- 填空题 -->
         <el-form-item v-if="temp.type === QuestionType.Completion || temp.type === QuestionType.QA || temp.type === QuestionType.ShortAnswer" :label="$t('AppQuestionBank[\'DisplayName:Answer\']')" prop="score" class="answer">
           <div v-if="temp.type === QuestionType.Completion">
-            <el-input v-model="temp.answer" placeholder="请输入内容">
-              <template slot="prepend">填空1答案</template>
-              <el-button v-if="false" slot="append" icon="el-icon-close" />
-            </el-input>
-            <el-input v-model="temp.answer" placeholder="请输入内容">
-              <template slot="prepend">填空2答案</template>
-              <el-button slot="append" icon="el-icon-close" />
+            <!-- 限制填空题答案输入框中不允许| 这个是答案拆分的分隔符 -->
+            <el-input v-for="(item, index) in completionAnswerArr" :key="item.key" v-model="item.value" placeholder="请输入内容">
+              <template slot="prepend">填空{{ index + 1 }}答案</template>
+              <el-button slot="append" icon="el-icon-close" @click="completionAnswerArr.splice(index,1)" />
             </el-input>
             <el-row>
-              <el-button type="primary" style="margin-right:10px;">增加填空题答案</el-button>
-              <el-checkbox v-model="checked">判分时不区分答案先后顺序</el-checkbox>
-              <el-checkbox v-model="checked">只要匹配答案的部分关键字就可得分</el-checkbox>
+              <el-button type="primary" style="margin-right:10px;" @click="addCompletionAnswer()">增加填空题答案</el-button>
+              <el-checkbox>判分时不区分答案先后顺序</el-checkbox>
+              <el-checkbox>只要匹配答案的部分关键字就可得分</el-checkbox>
             </el-row>
           </div>
           <div v-if="temp.type === QuestionType.QA || temp.type === QuestionType.ShortAnswer">
@@ -168,19 +165,21 @@ export default {
       questionTypeOptions: Type,
       questionDegreeOptions: Degree,
       alphas: ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'],
-      optionContentArr: ['', '', '', ''],
+      optionContentArr: ['', '', '', ''], // 选项数组
+      answerArr: [], // 多选题答案数组
+      completionAnswerArr: [], // 填空题答案数组
+      cursorIndex: '', // 光标位置
       radio: 'A',
       temp: {
         id: undefined,
         questionCategoryId: undefined,
         practicalTrainingId: undefined,
-        type: 2,
+        type: 1,
         name: '123',
         content: undefined,
         optionContent: undefined,
         optionSize: undefined,
         answer: undefined,
-        answerArr: [],
         score: undefined,
         degree: 1,
         analysis: undefined,
@@ -237,7 +236,7 @@ export default {
       // debugger
       this.optionContentArr.push({ value: '' })
     },
-
+    // 移除题目选项
     minusQuestionOption() {
       if (this.optionContentArr.length <= 4) {
         this.$message.error('至少保留4个选项！')
@@ -245,9 +244,39 @@ export default {
       }
       this.optionContentArr.pop({ value: '' })
     },
+    addCompletionAnswer() {
+      this.completionAnswerArr.push({ value: '' })
+    },
+    // 获取文本框的光标位置
+    onInputBlur(e) {
+      this.cursorIndex = e.srcElement.selectionStart
+    },
 
-    insertBlankFill() {
+    /**
+     * 点击按钮插入填空符
+     * 参考：element ui - el-input 实现点击插入关键词功能 https://blog.csdn.net/qq_38374286/article/details/132909372
+     * */
+    insertBlankFill(item = ' ___ ') {
+      // 将文本内容在光标位置进行拆分
+      const txt = this.temp.content ?? ''
+      const start = txt.substring(0, this.cursorIndex)
+      const end = txt.substring(this.cursorIndex, txt.length)
 
+      // 插入关键词
+      this.temp.content = start + `${item}` + end
+      this.completionAnswerArr.push({ value: '' })
+
+      // 获取文本框，设置焦点，处理光标位置
+      if (this.$refs[this.targetInputName]) {
+        this.$refs[this.targetInputName].focus()
+        this.$nextTick(() => {
+          const input = this.$refs[this.targetInputName].$el.firstElementChild
+          input.focus()
+          const addIndex = item.length
+          input.selectionStart = this.cursorIndex + addIndex
+          input.selectionEnd = this.cursorIndex + addIndex
+        })
+      }
     },
 
     // 重置表单
@@ -256,13 +285,12 @@ export default {
         id: undefined,
         questionCategoryId: undefined,
         practicalTrainingId: undefined,
-        type: 2,
+        type: 1,
         name: '123',
         content: undefined,
         optionContent: undefined,
         optionSize: undefined,
         answer: undefined,
-        answerArr: [],
         score: undefined,
         degree: 1,
         analysis: undefined,
@@ -277,6 +305,10 @@ export default {
         isShowImageButton: true,
         isShowLinkButton: true
       }
+
+      this.optionContentArr = ['', '', '', '']
+      this.answerArr = []
+      this.completionAnswerArr = [] // 填空题答案数组
     },
 
     // 点击创建按钮
@@ -299,10 +331,12 @@ export default {
             arr.push(e.value)
           })
           this.optionContent = arr.join('\r\n')
-          console.log('this.temp.QuestionType', this.temp.QuestionType)
           if (this.temp.type === QuestionType.MultipleChoice) {
             var answer1 = this.temp.answerArr.join('')
             this.temp.answer = answer1
+          }
+          if (this.temp.type === QuestionType.Completion) {
+            this.temp.answer = this.answerArr.join('|')
           }
           createQuestion(this.temp).then(() => {
             this.$emit('handleFilter', false)
@@ -326,7 +360,7 @@ export default {
 
       getQuestion(row.id).then(response => {
         this.temp = response
-        // TODO:重构代码，简化，变量重命名 题目选项拆分处理
+        // 拆分显示选项
         var tempOptionConentArr = response.optionContent.split(/[(\r\n)\r\n]+/) // 根据换行或者回车进行识别
         var resultArr = []
         if (tempOptionConentArr.length < 4) {
@@ -338,19 +372,22 @@ export default {
         }
         this.optionContentArr = resultArr
 
-        // if (this.temp.type === QuestionType.MultipleChoice) {
-        //   if (response.answer == null) {
-        //     response.answer = 'A'
-        //   }
-        //   // bug:Vue 动态生成 el-checkbox 点击无法赋值问题  https://www.cnblogs.com/ainyi/p/10409942.html
-        //   var checkList = response.answer.split('')
-        //   checkList.forEach(item => {
-        //     // debugger
-        //     const key = item.code
-        //     this.$set(this.temp.answerArr, item, item)
-        //   })
-        //   // this.temp.answerArr = response.answer.split('')
-        // }
+        // 多选题答案显示
+        if (this.temp.type === QuestionType.MultipleChoice) {
+          if (response.answer == null) {
+            response.answer = 'A'
+          }
+          var checkList = response.answer.split('')
+          this.answerArr = checkList
+        }
+
+        // 填空题答案显示
+        if (this.temp.type === QuestionType.Completion) {
+          var completionAnswerArr = response.answer.split('|')
+          completionAnswerArr.forEach(e => {
+            this.completionAnswerArr.push({ value: e })
+          })
+        }
       })
 
       // this.$nextTick(() => {
@@ -368,8 +405,10 @@ export default {
           })
           this.temp.optionContent = arr.join('\r\n')
           if (this.temp.type === QuestionType.MultipleChoice) {
-            var answer1 = this.temp.answerArr.join('')
-            this.temp.answer = answer1
+            this.temp.answer = this.answerArr.join('')
+          }
+          if (this.temp.type === QuestionType.Completion) {
+            this.temp.answer = this.answerArr.join('|')
           }
           updateQuestion(this.temp.id, this.temp).then(() => {
             this.$emit('handleFilter', false)
